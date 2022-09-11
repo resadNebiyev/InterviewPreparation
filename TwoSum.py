59e53bb6eb26d58fd9b9b3d6937cb2fa1011d784
Resaf <you@example.com>
2022-09-11 23:34:02 +0400

    added

--- TwoSum.py
+++ TwoSum.py
@@ -9,3 +9,14 @@ class Solution:
                     b = nums.index(nums[z],i+1)
                     return [a,b]
                     
+                    
+# 2-ci üsul
+                             
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:    
+#         for x in nums:   
+#             for y in nums[nums.index(x)+1:]:
+#                 if x + y == target:
+#                     print( [nums.index(x),nums.index(y,nums.index(x)+1)])
+#                     break
+            
